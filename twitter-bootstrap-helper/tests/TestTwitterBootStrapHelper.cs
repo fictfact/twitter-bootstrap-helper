4491755844307d3a727eafb994114a197ceb205f
Brian Kirsten <bkirsten@namemedia.com>
2013-05-15 16:59:54 -0400

    adding empty test

---
+++ twitter-bootstrap-helper/tests/TestTwitterBootStrapHelper.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+
+using twitterbootstraphelper;
+using twitterbootstraphelper.classes;
+
+namespace twitterbootstraphelper.tests
+{
+	[TestFixture()]
+	public class TestTwitterBootStrapHelper
+	{
+		[Test()]
+		public void TestCase ()
+		{
+			TwitterBootStrapHelper tbs = new TwitterBootStrapHelper();
+
+		}
+	}
+}
+
